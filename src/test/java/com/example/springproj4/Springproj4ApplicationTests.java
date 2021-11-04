5c85bd2f22d0ce28a3d3237ab636232640cde54f
Tahir <tahiryaqubov87@gmail.com>
2021-11-04 20:16:02 +0400

    updated

--- src/test/java/com/example/springproj4/Springproj4ApplicationTests.java
+++ src/test/java/com/example/springproj4/Springproj4ApplicationTests.java
@@ -1,60 +1,55 @@
 package com.example.springproj4;
-
+import org.json.JSONException;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.*;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.web.client.RestTemplate;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class Springproj4ApplicationTests {
 
-    @Autowired
-    BooksService booksService;
-
     @Test
-    void checkTitles() throws IOException {
-        String books = booksService.getBooks();
-
-        JSONArray results = new JSONArray(books);
-        for (int i = 0; i < results.length(); i++){
-            JSONObject obj1 = results.getJSONObject(i);
-            JSONArray bookDetails = obj1.getJSONArray("book_details");
-            String title = bookDetails.getJSONObject(0).getString("title");
-
-            System.out.println("Title: " + title);
-
-            assertThat(title).isNotEmpty();
+    public void check_title() throws JSONException {
+        String link = "https://api.nytimes.com/svc/books/v3/lists.json?list=Combined Print and E-Book Nonfiction&api-key=GRApLbPdvoGHc7QpCTGSAVSMURWA1mY8";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        ResponseEntity<String> out = restTemplate.exchange(link, HttpMethod.GET, entity, String.class);
+        JSONObject jsonObject = new JSONObject(out.getBody());
+        JSONArray jsonArray = jsonObject.getJSONArray("results");
+        boolean non_empty = true;
+        for (int n = 0; n < jsonArray.length(); n++){
+            String t = (String) jsonArray.getJSONObject(n).getJSONArray("book_details").getJSONObject(0).get("title");
+            if (t.length() == 0){
+                non_empty = false;
+            }
         }
+        assertTrue(non_empty);
     }
 
     @Test
-    void checkOrder() throws IOException {
-
-        String books = booksService.getBooks();
-        JSONArray results = new JSONArray(books);
-
-        ArrayList<Integer> rankArray = new ArrayList<Integer>();
-
-        for (int i = 0; i < results.length(); i++){
-            Integer rank = results.getJSONObject(i).getInt("rank");
-            rankArray.add(rank);
+    public void check_rank() throws JSONException {
+        String link = "https://api.nytimes.com/svc/books/v3/lists.json?list=Combined Print and E-Book Nonfiction&api-key=GRApLbPdvoGHc7QpCTGSAVSMURWA1mY8";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        ResponseEntity<String> out = restTemplate.exchange(link, HttpMethod.GET, entity, String.class);
+        JSONObject jsonObject = new JSONObject(out.getBody());
+        JSONArray jsonArray = jsonObject.getJSONArray("results");
+        boolean complete = true;
+        for (int n = 0; n < jsonArray.length(); n++){
+            Integer r = (Integer) jsonArray.getJSONObject(n).get("rank");
+            if (r != n + 1){
+                complete = false;
+            }
         }
-
-        Collections.sort(rankArray);
-
-        for (int i = 0; i < rankArray.size(); i++){
-            assertEquals(i+1, rankArray.get(i));
-        }
+        assertTrue(complete);
     }
 
 }
